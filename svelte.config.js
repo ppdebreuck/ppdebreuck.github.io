ad975b526c37897d2253c4c34c55d02c45dadc79
ppdebreuck <pierre-paul.debreuck@rub.de>
2025-12-03 16:17:50 +0100

    fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
+
 			fallback: '404.html',
+
 			precompress: false,
 			strict: true
 		}),
+
 		paths: {
 			base: ''
 		},
+
+		trailingSlash: 'always',
+
 		prerender: {
 			handleHttpError: 'warn'
 		}
